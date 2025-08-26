aff8ff465e3a55a36432659017959098cd137083
Temirlan Raiymbek <raiymbektemirlan25@gmail.com>
2025-08-27 01:01:18 +0500

    script

--- do_you_love_me.js
+++ do_you_love_me.js
@@ -1,25 +1,31 @@
 const questionContainer = document.querySelector(".question-container");
-const resultContainer = document.querySelector(".result-container.yes");
+const resultContainer = document.querySelector(".result-container");
 const gifResult = document.querySelector(".gif-result");
 const heartLoader = document.querySelector(".cssload-main");
 const yesBtn = document.querySelector(".js-yes-btn");
 const noBtn = document.querySelector(".js-no-btn");
-// /change the postion of no button
-noBtn.addEventListener("mouseover", () => {
-  const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
-  const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
+
+// функция для перемещения кнопки
+function moveNoBtn() {
+  const newX = Math.floor(Math.random() * (questionContainer.offsetWidth - noBtn.offsetWidth));
+  const newY = Math.floor(Math.random() * (questionContainer.offsetHeight - noBtn.offsetHeight));
 
   noBtn.style.left = `${newX}px`;
   noBtn.style.top = `${newY}px`;
-});
+}
+
+// при наведении
+noBtn.addEventListener("mouseover", moveNoBtn);
+
+// при клике
+noBtn.addEventListener("click", moveNoBtn);
 
 // yes button functionality
-
 yesBtn.addEventListener("click", () => {
   questionContainer.style.display = "none";
   heartLoader.style.display = "inherit";
 
-  const timeoutId = setTimeout(() => {
+  setTimeout(() => {
     heartLoader.style.display = "none";
     resultContainer.style.display = "inherit";
     gifResult.play();
